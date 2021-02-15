fd97e76381dc009d4ce23e8ee09c44ba9e05a3a7
tomson784 <tomohito040759@gmail.com>
2021-02-15 22:03:02 +0900

    first commit

---
+++ Assets/Scripts/Battle/BattleMain.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMain : BattleBase
+{
+
+    protected int _sceneTask;
+
+    // Use this for initialization
+    void Start()
+    {
+        // 配置するプレハブの読み込み 
+        GameObject prefab = (GameObject)Resources.Load("Objects/GrassTile");
+        // 配置元のオブジェクト指定 
+        GameObject stageObject = GameObject.FindWithTag("Stage");
+        // タイル配置
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+
+                Vector3 tile_pos = new Vector3(
+                    0 + prefab.transform.localScale.x * i,
+                    0,
+                    0 + prefab.transform.localScale.z * j
+
+                  );
+
+                if (prefab != null && i != 3 && j != 3)
+                {
+                    // プレハブの複製 
+                    GameObject instant_object =
+                      (GameObject)GameObject.Instantiate(prefab,
+                                                          tile_pos, Quaternion.identity);
+                    // 生成元の下に複製したプレハブをくっつける 
+                    instant_object.transform.parent = stageObject.transform;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
