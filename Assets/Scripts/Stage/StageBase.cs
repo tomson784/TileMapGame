6d71dd42838a5c3ec8a6abaabdce55fa45dd2ade
tomson784 <tomohito040759@gmail.com>
2021-02-16 16:45:44 +0900

    add stage base scripts

---
+++ Assets/Scripts/Stage/StageBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;  // 光線に当たったオブジェクトを受け取るクラス 
+        Ray ray;  // 光線クラス
+
+        // スクリーン座標に対してマウスの位置の光線を取得
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // マウスの光線の先にオブジェクトが存在していたら hit に入る 
+        if (Physics.Raycast(ray, out hit))
+        {
+            // 当たったオブジェクトのTileBaseクラスを取得
+            TileBase tile_base = hit.collider.GetComponent<TileBase>();
+            tile_base.bColorState = true;
+        }
+    }
+}
